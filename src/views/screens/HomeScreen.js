43cee6a1da80ad1b8e55cc413d2cbf3b870381f6
Your Name <matlalaj67@gmail.com>
2024-12-30 13:29:05 +0200

    Styling categories to show selected category

--- src/views/screens/HomeScreen.js
+++ src/views/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, View,TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from '../../consts/colors';
 import hotels from '../../consts/hotels';
@@ -7,25 +7,42 @@ import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 const HomeScreen = ({ navigation }) => {
     const categories = ['All', 'Popular', 'Top Rated', 'Featured', 'Luxury'];
+    const [selectedCategoryIndex,setSelectedCategoryIndex] = React.useState(0);
 
     const CategoryList = () => {
-        return(
+        return (
             <View style={style.categoryListContainer}>
-                {categories.map((item,index) => (
+                {categories.map((item, index) => (
                     <TouchableOpacity key={index} activeOpacity={0.8}>
                         <View>
-                            <Text>
+                            <Text 
+                            style={{...style.categoryListText,
+                                color:
+                                selectedCategoryIndex == index
+                                ? COLORS.primary 
+                                : COLORS.grey,
+                                 }}>
                                 {item}
                             </Text>
+                            {selectedCategoryIndex == index &&(
+                            <View style={{
+                                height: 3,
+                                width: 30,
+                                backgroundColor:COLORS.primary,
+                                marginTop:2,
+                            }}
+                            />
+                        )}
+
                         </View>
                     </TouchableOpacity>
                 ))}
-                 </View>
+            </View>
         );
     };
 
-return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
 
             <View style={style.header}>
                 <View style={{ paddingBottom: 15 }}>
@@ -34,24 +51,24 @@ return (
                     </Text>
 
                     <View style={{ flexDirection: 'row' }}>
-                        <Text style={{fontSize:30,fontWeight:'bold'}}>In
+                        <Text style={{ fontSize: 30, fontWeight: 'bold' }}>In
                         </Text>
-                        <Text style={{ fontSize: 30,  fontWeight: 'bold', color: COLORS.primary }}>
-                             Nokaneng
+                        <Text style={{ fontSize: 30, fontWeight: 'bold', color: COLORS.primary }}>
+                            Nokaneng
                         </Text>
                     </View>
                 </View>
-            <Icon name="person-outline" size={38} color={COLORS.grey}/>
+                <Icon name="person-outline" size={38} color={COLORS.grey} />
             </View>
             <ScrollView showsHorizontalScrollIndicator={false}>
                 <View style={style.searchInputContainer}>
-                    <Icon name="search" size={30} style={{marginLeft:20}} />
+                    <Icon name="search" size={30} style={{ marginLeft: 20 }} />
                     <TextInput
-                    placeholder="Search"
-                    style={{fontSize:20, paddingLeft:8}}/>
-                    
+                        placeholder="Search"
+                        style={{ fontSize: 20, paddingLeft: 8 }} />
+
                 </View>
-                <CategoryList/>
+                <CategoryList />
             </ScrollView>
         </SafeAreaView>
     );
@@ -64,7 +81,7 @@ const style = StyleSheet.create({
         justifyContent: 'space-between',
         paddingHorizontal: 20,
     },
-    searchInputContainer:{
+    searchInputContainer: {
         height: 50,
         backgroundColor: COLORS.light,
         marginTop: 15,
@@ -74,13 +91,15 @@ const style = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
     },
-    categoryListContainer:{
-        flexDirection:'row',
+    categoryListContainer: {
+        flexDirection: 'row',
         justifyContent: 'space-between',
-        marginHorizontal:20,
+        marginHorizontal: 20,
         marginTop: 30,
-
-
+    },
+    categoryListText:{
+        fontSize:17,
+        fontWeight:'bold',
     }
 });
 
