45820d26f8235a9435ca1cfca47e718b5be23e7f
Your Name <matlalaj67@gmail.com>
2025-01-09 17:44:09 +0200

    Added another view for Top Restuarants and navigation to Details Page

--- src/views/screens/HomeScreen.js
+++ src/views/screens/HomeScreen.js
@@ -13,7 +13,7 @@ const HomeScreen = ({ navigation }) => {
   const [selectedCategoryIndex, setSelectedCategoryIndex] = React.useState(0);
   const scrollX = React.useRef(new Animated.Value(0)).current;
 
-  const CategoryList = () => {
+  const CategoryList = ({navigation}) => {
     return (
       <View style={style.categoryListContainer}>
         {categories.map((item, index) => (
@@ -48,7 +48,8 @@ const HomeScreen = ({ navigation }) => {
     });
 
     return (
-      <Animated.View style={{ ...style.card, transform: [{ scale }] }}>
+      <TouchableOpacity activeOpacity={1} onPress={()=>navigation.navigate('DetailsScreen',hotel)} >
+<Animated.View style={{ ...style.card, transform: [{ scale }] }}>
         <Animated.View style={{ ...style.cardOverLay, opacity }} />
 
         <View style={style.priceTag}>
@@ -84,11 +85,27 @@ const HomeScreen = ({ navigation }) => {
           </View>
         </View>
       </Animated.View>
-    );
+
+      </TouchableOpacity>
+          );
   };
 
   const TopHotelCard = ({ hotel }) => {
-    return <View style={style.TopHotelCard}></View>;
+    return <View style={style.TopHotelCard}>
+      <View style={{position:'absolute',top:5,right:5,zIndex:1,flexDirection:'row'}}>
+        <Icon name="star" size={15} color={COLORS.orange} />
+        <Text style={{color: COLORS.white, fontWeight:'bold',fontSize:15}}>
+          5.0
+        </Text>
+      </View>
+          <Image style={style.TopHotelCardImage} source={hotel.image}/>
+          <View style={{paddingVertical:5,paddingHorizontal:10}}>
+            <Text style={{fontSize:10, fontWeight:'bold'}}>{hotel.name} </Text>
+            <Text style={{fontSize:7, fontWeight:'bold',color:COLORS.grey}}>
+              {hotel.name}
+            </Text>
+          </View>
+    </View>;
   };
 
   return (
@@ -135,16 +152,18 @@ const HomeScreen = ({ navigation }) => {
 
         }}>
           <Text style={{ color: COLORS.grey, fontWeight: 'bold' }}>
-            Top Hotels
+            Top Restaurants
           </Text>
           <Text style={{ color: COLORS.grey }}> Show all</Text>
         </View>
-        <FlatList horizontal
+        <FlatList
+        data={hotels}
+        horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={{
             paddingLeft: 20,
             marginTop: 20,
-            paddingBottom: 30
+            paddingBottom: 30,
           }}
           renderItem={({ item }) => <TopHotelCard hotel={item} />}
         />
