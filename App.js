1bb226c5dd6998b42fef8582cea9961619d9eab3
DopeGrammerZA <Kagisomasebe98@gmail.com>
2025-01-11 12:08:02 +0200

    Created Register and Login Sections

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import COLORS from './src/consts/colors';
 import DetailsScreen from './src/views/screens/DetailsScreen';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Navigator } from 'react-native';
+import LoginScreen from './src/views/screens/LoginScreen';
+import RegisterScreen from './src/views/screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +18,8 @@ return (
 <NavigationContainer>
   <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
   <Stack.Navigator screenOptions={{headerShown:false}}>
+    <Stack.Screen name="RegisterScreen" component ={RegisterScreen} />
+    <Stack.Screen name="LoginScreen" component ={LoginScreen} />
     <Stack.Screen name="Home" component ={HomeScreen} />
     <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
     </Stack.Navigator>  
